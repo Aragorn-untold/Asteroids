1262baec973da6c2bf4cafb4b4a045b4823e3fe1
Aragorn-untold <idytkoni@gmail.com>
2026-06-29 09:30:44 +0000

    added shoot function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -16,12 +17,14 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    shots = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()   
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
 
     aster_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
